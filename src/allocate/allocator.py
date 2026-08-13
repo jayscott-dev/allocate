4e66a3183ea6b60ece734dda0f53b2017cca7c26
Jayden Scott <jayscott.ld@gmail.com>
2026-08-13 18:45:26 -0500

    read budgets file

---
+++ src/allocate/allocator.py
@@ -0,0 +1,13 @@
+from allocate.models import Budget
+from pathlib import Path
+import json
+
+def allocate_budget(total: int, budgets_file: Path):
+    with budgets_file.open("r") as f:
+        raw_data = json.loads(f.read())
+
+    budgets = [Budget.from_dict(budget) for budget in raw_data.get("budgets", [])]
+
+    for b in budgets:
+        print(b.name)
+    
